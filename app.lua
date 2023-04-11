b5117c374af892017a5aab0491b23c78e491a528
ropoko <rodrigostramantinoli@gmail.com>
2023-04-11 18:24:18 -0300

    feat: get username

--- app.lua
+++ app.lua
@@ -1,8 +1,15 @@
 local lapis = require('lapis')
+local db = require('lapis.db')
+
 local app = lapis.Application()
 
 app:get('/', function()
-  return 'Welcome to Lapis ' .. require('lapis.version')
+	local res = table.unpack(db.query('SELECT username FROM account where username = ?', 'ropoko'))
+
+	-- TODO: should I use this as implementation for ternary operator?
+	local username = (function() if res ~= nil then return res.username else return 'anonymous' end end)()
+
+  return 'Welcome to Lapis: '..username
 end)
 
 return app
