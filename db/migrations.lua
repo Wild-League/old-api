5d53b763b0f37a300694dd58638bcf4f07a83757
ropoko <rodrigostramantinoli@gmail.com>
2023-04-13 01:07:05 -0300

    feat: added default value for type in account

--- db/migrations.lua
+++ db/migrations.lua
@@ -7,12 +7,15 @@ return {
 		schema.create_table('account', {
 			{ 'id', types.serial, primary_key = true },
 			{ 'username', 'character varying(20)' }, -- https://github.com/leafo/lapis/issues/591
-			{ 'type', 'character varying(20)' },
+			{ 'display_name', 'character varying(20)' },
+			{ 'bio', 'character varying(500)' },
+			{ 'uri', types.varchar },
 			{ 'url', types.varchar },
+			{ 'type', types.text({ default='Person' }) },
 			{ 'inbox_url', types.varchar },
 			{ 'outbox_url', types.varchar },
-			{ 'display_name', 'character varying(20)' },
-			{ 'icon', types.varchar }
+			{ 'icon', types.varchar },
+			{ 'created_at', types.date }
 		})
 	end
 }
