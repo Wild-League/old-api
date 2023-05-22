bd3d277a211cc9b49f7a08e010f95bdbbf45f626
ropoko <rodrigostramantinoli@gmail.com>
2023-05-22 14:20:51 -0300

    feat: added new table posts

--- db/migrations.lua
+++ db/migrations.lua
@@ -6,10 +6,10 @@ return {
 	[1] = function()
 		schema.create_table('accounts', {
 			{ 'id', types.serial, primary_key = true },
-			{ 'username', 'character varying(25)' }, -- https://github.com/leafo/lapis/issues/591
-			{ 'display_name', 'character varying(85)' },
+			{ 'username', 'character varying(25) not null' }, -- https://github.com/leafo/lapis/issues/591
+			{ 'display_name', 'character varying(85) not null' },
 			{ 'bio', 'character varying(500)' },
-			{ 'domain', 'character varying(60)' },
+			{ 'domain', 'character varying(60) not null' },
 			{ 'short_url', types.varchar },
 			{ 'long_url', types.varchar },
 			{ 'type', types.text({ default='Person' }) },
@@ -18,10 +18,10 @@ return {
 			{ 'icon', types.varchar },
 			{ 'created_at', types.date },
 			{ 'level', types.numeric({ default=1 }) },
-			{ 'email', 'character varying(100)' },
-			{ 'password', 'character varying(100)' },
+			{ 'email', 'character varying(100) not null' },
+			{ 'password', 'character varying(100) not null' },
 			{ 'agreement', types.boolean }
-		})
+		}, { if_not_exists = true } )
 	end,
 	[2] = function()
 		schema.create_table('rules', {
@@ -29,6 +29,18 @@ return {
 			{ 'text', types.varchar },
 			{ 'created_at', types.date },
 			{ 'updated_at', types.date({ null = true }) },
-		})
+		}, { if_not_exists = true } )
 	end,
+	[3] = function()
+		schema.create_table('posts', {
+			{ 'id', types.serial, primary_key = true },
+			{ 'uri', types.varchar },
+			{ 'created_at', types.date },
+			{ 'edited_at', types.date({ null = true }) },
+			{ 'account_id', types.numeric },
+			{ 'content', types.varchar },
+			{ 'in_reply_to_post_id', types.numeric({ null = true }) },
+			{ 'in_reply_to_account_id', types.numeric({ null = true }) },
+		}, { if_not_exists = true } )
+	end
 }
