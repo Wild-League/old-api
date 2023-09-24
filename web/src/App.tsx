1612d1682707d50d23ce9d88d4939207c255d352
ropoko <rodrigostramantinoli@gmail.com>
2023-09-24 15:46:58 -0300

    feat: adding waitlist page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './styles/index.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+			<h1>Wild League</h1>
+			<p>Gaming experience across the Fediverse.</p>
+
+			<div>
+				<p>Gaming isn't just about playing</p>
+				<p>Gaming is about <span>community</span> </p>
+			</div>
+
+			<form>
+				<label htmlFor='email'>Join the waitlist:</label>
+				<input id='email' type="email" placeholder='email' />
+				<button type='submit'> Notify me when you launch </button>
+			</form>
+		</main>
   )
 }
 
