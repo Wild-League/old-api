6eb06dbb0bf814ceeb0f29e5a457ba89c48775e2
ropoko <rodrigostramantinoli@gmail.com>
2023-11-14 23:43:31 -0300

    feat: wip sending email

--- src/services/email_service.lua
+++ src/services/email_service.lua
@@ -1,8 +1,9 @@
 local https = require('ssl.https')
 local ltn12 = require('ltn12')
+local json = require('lib.json')
 
 local EmailService = {
-	api_token = 're_ZuiG4zyx_8V2oNJWJWWBG7XNXUhGS8BJn'
+	api_token = 'd0129e2f-5675-4b3a-997d-a7df0d802a61'
 }
 
 function EmailService:send()
@@ -10,15 +11,27 @@ function EmailService:send()
 
 	local response = {}
 
-	https.request({
-		url = 'https://httpbin.org/get',
-		sink = ltn12.sink.table(response),
-		method = 'GET'
+	local body = json.encode({
+		['From'] = 'contact@wildleague.org',
+		['To'] = 'contact@wildleague.org',
+		['Subject'] = 'Hello world!',
+		['TextBody'] = 'Hello World Email Test'
 	})
 
-	for key, value in pairs(response) do
-		print(key, value)
-	end
+	local headers = {
+		['X-Postmark-Server-Token'] = self.api_token,
+		['Content-Type'] = 'application/json',
+		['Accept'] = 'application/json',
+		["Content-Length"] = string.len(body)
+	}
+
+	https.request({
+		url = 'https://api.postmarkapp.com/email',
+		source = ltn12.source.string(body),
+		sink = ltn12.sink.table(response),
+		method = 'POST',
+		headers = headers
+	})
 end
 
 return EmailService
