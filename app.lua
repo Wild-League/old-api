5521782d0aa1a2f6aa74b441ef60b3a09d10641d
ropoko <rodrigostramantinoli@gmail.com>
2023-11-27 20:09:55 -0300

    chore: test gh action

--- app.lua
+++ app.lua
@@ -4,9 +4,9 @@ local api = require('src.api.api')
 local well_known = require('src.well_known.well_known')
 
 local app = lapis.Application()
+
 app:enable("etlua")
 app.views_prefix = 'static.views'
-
 
 app:include(api, { name='api' })
 app:include(well_known, { name='well_known' })
